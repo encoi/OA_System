503ab4f31fe0dce067ac861e31815049d4e683ba
ZhangXu <zxkl1314@gmail.com>
2024-05-20 14:06:35 +0800

    创建dept实体类

--- src/main/java/com/team/oa_system/entity/dept.java
+++ src/main/java/com/team/oa_system/entity/dept.java
@@ -1,4 +1,17 @@
 package com.team.oa_system.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class dept {
+    private Integer id;
+    private String name;
+    private LocalDate creatTime;
+    private LocalDate updateTime;
+
 }
